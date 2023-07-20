0893583d58f4ab6bd2e0a9d2dc7bfab2f7446134
MaximZykin <zikinmax95@mail.ru>
2023-07-20 09:35:31 +0700

    fix

--- Food order/Services/NetworkManager.swift
+++ Food order/Services/NetworkManager.swift
@@ -12,7 +12,7 @@ class NetworkManager {
     
     private let apiCategory = "https://run.mocky.io/v3/058729bd-1402-4578-88de-265481fd7d54"
     private let apiDishes = "https://run.mocky.io/v3/aba7ecaa-0a70-453b-b62d-0e326c859b3b"
-    
+     
     func fetchData(completion: @escaping (_ category: [Сategory]) -> Void) {
         guard let url = URL(string: apiCategory) else { return }
         
